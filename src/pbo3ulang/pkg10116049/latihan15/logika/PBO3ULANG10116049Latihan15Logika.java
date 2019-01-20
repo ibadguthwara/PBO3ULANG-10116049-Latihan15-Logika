ba1d7c3620341061bd00bfe2dd218f9b05fe7e57
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-01-20 23:38:04 +0700

    Initial commit

---
+++ src/pbo3ulang/pkg10116049/latihan15/logika/PBO3ULANG10116049Latihan15Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan15.logika;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA 	     : M. Ibad Guthwara
+ * KELAS	     : PBO 3 ULANG
+ * NIM		     : 10116049
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk operasi logika
+ */
+public class PBO3ULANG10116049Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        
+        System.out.println("a && b = " + (a&&b));
+        System.out.println("a || b = " + (a||b));
+        System.out.println("!a = " + (!a));
+        System.out.println("!(a && b) = " + !(a&&b));
+    }
+    
+}
